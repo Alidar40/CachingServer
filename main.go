45e9c3b9a1e89745dcbb0a228a40627dc11949a2
Alidar Asvarov <alidar.asvarov@gmail.com>
2018-12-20 23:54:29 +0300

    Added: logging and error handling middlewares; Changed: db structure and file storage method, hence all API methods were revised

--- main.go
+++ main.go
@@ -2,26 +2,32 @@ package main
 
 import(
 	"fmt"
-	"log"
 	"net/http"
 	"strconv"
 	"database/sql"
 	"encoding/json"
 	"sync"
-	"errors"
-	"bytes"
+	"io/ioutil"
+	"os"
+	"time"
+	"flag"
 
 	"github.com/gocraft/web"
 	_ "github.com/lib/pq"
+	"github.com/golang/glog"
+	"github.com/pkg/errors"
 )
 
 type Doc struct {
-	Id	int	`json:"id"`
+	Id	string	`json:"id"`
 	Name	string	`json:"name"`
-	Content string	`json:"content"`
+	Mime	string	`json:"mime"`
+	Public	bool	`json:"public"`
+	Created	string	`json:"created"`
 }
 
 type Docs struct {
+	Filename	string	`json:"file,omitempty"`
 	DocsList	[]*Doc	`json:"docs,omitempty"`
 }
 
@@ -34,17 +40,80 @@ type Response struct {
 	//TODO(Alidar) Fill this
 }
 
-type ReplyData struct {
-	//TODO(Alidar) Fill this
-}
-
 type ReplyModel struct {
-	Err	ReplyError	`json:"error"`
-	Res	Response	`json:"responce"`
-	Data	ReplyData	`json:"data"`
+	Err	*ReplyError	`json:"error,omitempty"`
+	Res	*Response	`json:"responce,omitempty"`
+	Data	*Docs		`json:"data,omitempty"`
 }
 
 type Context struct {
+	Error	error
+}
+
+func (c *Context) Log(rw web.ResponseWriter, req *web.Request, next web.NextMiddlewareFunc){
+	start := time.Now()
+	next(rw, req)
+	glog.Infof("[%s] [%s %s]", time.Since(start), req.Method, req.URL)
+}
+
+func (c *Context) HandleError(rw web.ResponseWriter, req *web.Request, next web.NextMiddlewareFunc){
+	next(rw, req)
+
+	if (c.Error != nil) {
+		glog.Infof("[ERROR] [%s %s] while %s", req.Method, req.URL, c.Error)
+		var code int
+		var text string
+
+		switch rw.StatusCode() {
+			case 400:
+				code = 400
+				text = "Bad request"
+			case 401:
+				code = 401
+				text = "Unauthorized"
+			case 403:
+				code = 403
+				text = "Forbidden"
+			case 404:
+				code = 404
+				text = "Not found"
+			case 405:
+				code = 405
+				text = "Method not allowed"
+			case 500:
+				code = 500
+				text = "Internal server error"
+			case 501:
+				code = 501
+				text = "Not implemented"
+			case 503:
+				code = 503
+				text = "Service unavailable"
+			default:
+				code = 500
+				text = "Internal server error"
+		}
+
+		reply := &ReplyModel{
+			Err: &ReplyError{
+				Code: code,
+				Text: text,
+			},
+		}
+
+		c.Reply(rw, req, reply)
+		return
+	}
+}
+
+func (c *Context) Reply(rw web.ResponseWriter, req *web.Request, model *ReplyModel){
+	reply, err := json.MarshalIndent(model, "	", "	")
+	if (err != nil) {
+		c.Error = err
+		return
+	}
+	rw.Header().Set("Content-Type", "application/json")
+	rw.Write(reply)
 }
 
 var(
@@ -76,22 +145,28 @@ func ReadFromCache(key string) (Doc, error){
 	data, ok := cache[key]
 	if (ok == false){
 		mutex.Unlock()
-		return data, errors.New("No such a value in cache")
+		return data, errors.New("no such a value in cache")
 	}
 	mutex.Unlock()
 	return data, nil
 }
 
 func main() {
+	flag.Set("logtostderr", "true")
+	flag.Set("v", "2")
+	flag.Parse()
+
 	var err error
 	db, err = sql.Open("postgres", "postgres://alidar:1@localhost/cachingserverdb");
 	if (err != nil){
-		log.Println("[ERR] Opening DB: ", err)
+		glog.Infof("[ERROR] in main(!) while opening db: %s", err)
 		return
 	}
 	defer db.Close()
 
-	router := web.New(Context{}).Middleware(web.LoggerMiddleware)
+	router := web.New(Context{}).
+		Middleware((*Context).Log).
+		Middleware((*Context).HandleError)
 	router.Get("/", (*Context).RootRoute)
 	router.Get("/docs", (*Context).GetDocsRoute)
 	router.Get("/docs/:id", (*Context).GetDocByIdRoute)
@@ -104,43 +179,70 @@ func (c *Context) RootRoute(rw web.ResponseWriter, req *web.Request){
 }
 
 func (c *Context) PostDocRoute(rw web.ResponseWriter, req *web.Request){
-	//TODO(Alidar): Check for max value
-	err := req.ParseMultipartForm(0)
+	err := req.ParseMultipartForm(16777216) //16 MiB
 	if (err != nil) {
-		log.Println("[ERR] POST /docs while parsing form: ", err)
+		c.Error = errors.Wrap(err, "parsing form")
+		rw.WriteHeader(http.StatusBadRequest)
 		return
 	}
 
 	file, fileHeader, err := req.FormFile("newdoc")
 	if (err != nil) {
-		log.Println("[ERR] POST /docs while parsing file: ", err)
+		c.Error = errors.Wrap(err, "parsing file")
+		rw.WriteHeader(http.StatusBadRequest)
 		return
 	}
 	defer file.Close()
 
-	var content = make([]byte, 1048576) //1MiB
-	_, err = file.Read(content)
+	content, err  := ioutil.ReadAll(file)
 	if (err != nil) {
-		log.Println("[ERR] POST /docs while reading file: ", err)
+		c.Error = errors.Wrap(err, "reading file")
+		rw.WriteHeader(http.StatusBadRequest)
 		return
 	}
 
-	content = bytes.Trim(content, "\x00")
-	_, err = db.Exec("INSERT INTO docs (name, content) VALUES ($1, $2);", fileHeader.Filename, string(content))
+	_, err = os.Stat("./UserFiles/" + fileHeader.Filename)
+	if (os.IsNotExist(err)) {
+		err = ioutil.WriteFile("./UserFiles/" + fileHeader.Filename, content, os.ModePerm)
+		if (err != nil) {
+			c.Error = errors.Wrap(err, "saving file on disk")
+			rw.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+	} else {
+		err = errors.New("file already exists")
+		c.Error = errors.Wrap(err, "creating already existing file")
+		rw.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	var name = fileHeader.Filename
+	var mime = fileHeader.Header.Get("Content-Type")
+	var public = req.FormValue("isPublic")
+	_, err = db.Exec(`
+		INSERT INTO docs (name, mime, public) 
+		VALUES ($1, $2, $3);`, name, mime, public)
 	if (err != nil) {
-		log.Println("[ERR] POST /docs while inserting into db: ", err)
+		c.Error = errors.Wrap(err, "inserting into db")
+		rw.WriteHeader(http.StatusInternalServerError)
 		return
 	}
 
 	SetCacheIrrelevant()
+
+	reply := &ReplyModel{
+		Data: &Docs{
+			Filename: name,
+		},
+	}
+
+
 	rw.WriteHeader(http.StatusCreated)
-	rw.Write([]byte("{\"file\":\"" + fileHeader.Filename + "\""))
+	c.Reply(rw, req, reply)
 }
 
 func (c *Context) GetDocsRoute(rw web.ResponseWriter, req *web.Request){
 	limit := req.URL.Query().Get("limit")
-	key := req.URL.Query().Get("key")
-	value := req.URL.Query().Get("value")
 
 	var countOfDocs int
 	var err error
@@ -150,51 +252,48 @@ func (c *Context) GetDocsRoute(rw web.ResponseWriter, req *web.Request){
 			countOfDocs = 0
 		}
 	}
-
-	if (key != "") {
-		if (key != "name" || key != "id" || key != "created") {
-			key = "name"
+	if (cacheIsRelevant) {
+		docs := new(Docs)
+		if (countOfDocs == 0){
+			for _, value := range cache {
+				var doc = value
+				docs.DocsList = append(docs.DocsList, &doc)
+			}
+		} else {
+			var i = 0
+			for _, value := range cache {
+				var doc = value
+				docs.DocsList = append(docs.DocsList, &doc)
+				i++
+				if (i == countOfDocs) {
+					break
+				}
+			}
 		}
-	} else {
-		key = "name"
-	}
-
-	if (value != "") {
-		if (value != "asc" || value != "desc") {
-			value = "asc"
+		reply := &ReplyModel{
+			Data: docs,
 		}
-	} else {
-		value = "asc"
-	}
-
-	if (cacheIsRelevant){
-		//TODO(Alidar): limit number of docs responded
-		marshaledCache, err := json.Marshal(cache)
-		if (err != nil) {
-			log.Println("[ERR] GET /doc while marshaling cache")
-			SetCacheIrrelevant()
-			return
-		}
+		SetCacheRelevant()
 		rw.WriteHeader(http.StatusOK)
-		rw.Write([]byte(marshaledCache))
-	} else{
+		c.Reply(rw, req, reply)
+	} else {
 		var result *sql.Rows
 		if (countOfDocs == 0){
 			result, err = db.Query(`
-				SELECT id, name, content 
-				FROM docs
-				ORDER BY ` +  key + ` ` +  value)
+				SELECT id, name, mime, public, created_at
+				FROM docs`)
 
 		} else {
 			result, err = db.Query(`
-				SELECT id, name, content 
+				SELECT id, name, mime, public, created_at
 				FROM docs 
-				ORDER BY ` +  key + ` ` +  value + `
 				LIMIT $1`, countOfDocs)
 		}
 
 		if (err != nil){
-			log.Println("[ERR] GET /docs while querying: ", err)
+			SetCacheIrrelevant()
+			c.Error = errors.Wrap(err, "querying")
+			rw.WriteHeader(http.StatusInternalServerError)
 			return
 		}
 		defer result.Close()
@@ -202,81 +301,57 @@ func (c *Context) GetDocsRoute(rw web.ResponseWriter, req *web.Request){
 		docs := new(Docs)
 		for result.Next(){
 			doc := new(Doc)
-			err := result.Scan(&doc.Id, &doc.Name, &doc.Content)
+			err = result.Scan(&doc.Id, &doc.Name, &doc.Mime, &doc.Public, &doc.Created)
 			if (err != nil){
-				log.Println("[ERR] GET /doc while scanning result: ", err)
+				SetCacheIrrelevant()
+				c.Error = errors.Wrap(err, "scanning result")
+				rw.WriteHeader(http.StatusInternalServerError)
 				return
 			}
 			docs.DocsList = append(docs.DocsList, doc)
 
-			WriteToCache(strconv.Itoa(doc.Id), *doc)
+			WriteToCache(doc.Id, *doc)
 		}
 
 		if err = result.Err(); err != nil {
-			log.Println("[ERR] GET /doc after delving in result: ", err)
 			SetCacheIrrelevant()
+			c.Error = errors.Wrap(err, "after delving in result")
+			rw.WriteHeader(http.StatusInternalServerError)
 			return
 		}
+
 		SetCacheRelevant()
 
-		response, err := json.Marshal(docs)
-		if (err != nil) {
-			log.Println("[ERR] GET /doc while marshaling docs")
-			SetCacheIrrelevant()
-			return
+		reply := &ReplyModel{
+			Data: docs,
 		}
-		rw.WriteHeader(http.StatusOK)
-		rw.Write(response)
-	}
 
+		rw.WriteHeader(http.StatusOK)
+		c.Reply(rw, req, reply)
+	}
 }
 
 func (c *Context) GetDocByIdRoute(rw web.ResponseWriter, req *web.Request){
-	//TODO(Alidar): id needs to be validated
 	var docId = req.PathParams["id"]
 
-	if (cacheIsRelevant) {
-		marshaledCache, err := json.Marshal(cache[docId])
-		if (err != nil) {
-			log.Println("[ERR] GET /doc while marshaling cache")
-			SetCacheIrrelevant()
-			return
-		}
-		rw.WriteHeader(http.StatusOK)
-		rw.Write([]byte(marshaledCache))
+	doc, ok := cache[docId]
+	if ok {
+		rw.Header().Set("Content-Type", doc.Mime)
+		http.ServeFile(rw, req.Request, "./UserFiles/" + doc.Name)
 	} else {
-		//TOFIGUREOUT(Alidar): Should I retrieve ALL data from db in order to update cache??
-		result, err := db.Query("SELECT id, name, content FROM docs WHERE id=$1", docId)
-		if (err != nil){
-			log.Println("[ERR] GET /doc:id while querying: ", err)
-			return
-		}
-		defer result.Close()
-
 		doc := new(Doc)
-		for result.Next(){
-			err := result.Scan(&doc.Id, &doc.Name, &doc.Content)
-			if (err != nil){
-				log.Println("[ERR] GET /doc:id while scanning result: ", err)
-				return
-			}
-			break
-		}
+		err := db.QueryRow("SELECT id, name, mime, public, created_at FROM docs WHERE id=$1", docId).
+			Scan(&doc.Id, &doc.Name, &doc.Mime, &doc.Public, &doc.Created)
 
-		if err = result.Err(); err != nil {
-			log.Println(err)
-		}
-		response, err := json.Marshal(doc)
-		if (err != nil) {
-			log.Println("[ERR] GET /doc/:id while marshaling result: ", err)
+		if (err != nil){
+			c.Error = errors.Wrap(err, "querying file")
+			rw.WriteHeader(http.StatusNotFound)
 			return
 		}
 
-		rw.WriteHeader(http.StatusOK)
-		rw.Write(response)
+		WriteToCache(doc.Id, *doc)
 
+		rw.Header().Set("Content-Type", doc.Mime)
+		http.ServeFile(rw, req.Request, "./UserFiles/" + doc.Name)
 	}
-
 }
-
-
